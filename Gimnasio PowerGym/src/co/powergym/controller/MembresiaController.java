47f4b4e53331c154da537b92e37bc16d9a35beb2
Cristian Cruz <criscruz1806@gmail.com>
2017-06-10 02:08:20 -0500

    Diseño membresias crear

--- Gimnasio PowerGym/src/co/powergym/controller/MembresiaController.java
+++ Gimnasio PowerGym/src/co/powergym/controller/MembresiaController.java
@@ -8,6 +8,8 @@ import co.powergym.view.CrearMembresia;
 public class MembresiaController implements ActionListener {
 
 	private CrearMembresia membresia;
+	int contPasos = 1;
+
 	
 	public MembresiaController(CrearMembresia membresia) {
 		
@@ -16,19 +18,45 @@ public class MembresiaController implements ActionListener {
 		this.membresia.getBtnSiguiente().addActionListener(this);
 		this.membresia.getBtnAtras().addActionListener(this);
 	}
-
 	@Override
 	public void actionPerformed(ActionEvent e) {
+	
+	
 		
-		int contPasos = 1;
 		if(e.getSource() == membresia.getBtnSiguiente()){
 			
-			membresia.getLbl_pregunta_1().setVisible(false);
-			membresia.getLbl_pregunta_2().setBounds(20, 118, 285, 16);
-			contPasos ++;
+			if (contPasos == 1) {
+				contPasos ++;
+				membresia.getLblPasoDe().setText("Paso "+contPasos+" de 4");
+				
+				
+				membresia.getLbl_pregunta_1().setVisible(false);
+				membresia.getTfNombreMembresia().setVisible(false);
+				
+				membresia.getLbl_pregunta_2().setBounds(20, 118, 285, 16);
+				membresia.getBtnAtras().setBounds(22, 280, 117, 37);
+				membresia.getLblCantidad().setBounds(21, 146, 61, 16);
+				membresia.getCBXCantidad().setBounds(92, 142, 61, 27);
+				membresia.getCBXTipoTiempo().setBounds(156, 142, 156, 27);
+			}else 
+			if (contPasos == 2) {
+				
+				contPasos ++;
+				membresia.getLblPasoDe().setText("Paso "+contPasos+" de 4");
+				membresia.getLbl_pregunta_2().setVisible(false);
+				membresia.getLb_pregunta_3().setBounds(20, 118, 342, 16);
+				
+			} else
+			if (contPasos == 3) {
+				
+				contPasos ++;
+				membresia.getLblPasoDe().setText("Paso "+contPasos+" de 4");
+				
+				
+			}
 		}
 		if(e.getSource() == membresia.getBtnAtras()){
-			membresia.setVisible(false);
+			
 		}
 	}
 
