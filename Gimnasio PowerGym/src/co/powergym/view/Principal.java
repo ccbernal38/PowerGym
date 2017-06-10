82ed142e2d9db904b805695b5ac4309f5e68cca8
bernal_9219@hotmail.com <cbernal@apliko.com.co>
2017-06-10 02:15:41 -0500

    cambios

--- Gimnasio PowerGym/src/co/powergym/view/Principal.java
+++ Gimnasio PowerGym/src/co/powergym/view/Principal.java
@@ -303,17 +303,7 @@ public class Principal extends JFrame {
         // TODO add your handling code here:
     }//GEN-LAST:event_jButton3ActionPerformed
 
-    private Image getScaledImage(Image srcImg, int w, int h){
-        BufferedImage resizedImg = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
-        Graphics2D g2 = resizedImg.createGraphics();
-
-        g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
-        g2.drawImage(srcImg, 0, 0, w, h, null);
-        g2.dispose();
-
-        return resizedImg;
-    }
-   
+    
     // Variables declaration - do not modify//GEN-BEGIN:variables
     public javax.swing.JButton btnRegistrarSocio;
     public javax.swing.JButton jButton2;
