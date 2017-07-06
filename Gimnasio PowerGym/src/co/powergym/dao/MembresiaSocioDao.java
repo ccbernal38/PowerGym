2e2e957b00c0561066a3911086bb59c8f1329604
bernal_9219@hotmail.com <cbernal@apliko.com.co>
2017-07-05 22:42:32 -0500

    Lista de membresias

--- Gimnasio PowerGym/src/co/powergym/dao/MembresiaSocioDao.java
+++ Gimnasio PowerGym/src/co/powergym/dao/MembresiaSocioDao.java
@@ -30,30 +30,7 @@ public class MembresiaSocioDao implements MembresiaSocioDaoInterface {
 			Connection accesoBD = conexion.getConexion();
 			PreparedStatement statement = accesoBD
 					.prepareStatement("INSERT INTO MembresiaSocio(Socio_id, Membresia_id, ) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?)");
-			statement.setString(1, identificacion);
-			statement.setString(2, primerNombre);
-			statement.setString(3, segundoNombre);
-			statement.setString(4, primerApellido);
-			statement.setString(5, segundoApellido);
-			statement.setDate(6, fechaNacimiento);
-			statement.setString(7, telefono);
-			statement.setString(8, correo);
-			statement.setInt(9, genero);
-			if (foto != null) {
-				ByteArrayOutputStream baos = new ByteArrayOutputStream();
-				ImageIO.write(foto, "jpg", baos);
-				byte[] imageInByte = baos.toByteArray();
-				Blob blob = accesoBD.createBlob();
-				blob.setBytes(1, imageInByte);
-				statement.setBlob(10, blob);
-			}
-
-			Calendar calendar = Calendar.getInstance();
-			calendar.setTime(fechaNacimiento);
-
-			statement.setInt(11, calendar.get(Calendar.DAY_OF_MONTH));
-			statement.setInt(12, calendar.get(Calendar.MONTH) + 1);
-			statement.setInt(13, calendar.get(Calendar.YEAR));
+			
 			statement.execute();
 			respuesta = true;
 		} catch (SQLException e) {
